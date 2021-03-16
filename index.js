15c38c7b1ee575369c4cfac85e48332e2f07d7fa
AshrafLobo <ashraflobo@gmail.com>
2021-03-16 16:45:23 +0300

    Minor changes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-require('dotenv').config()
+
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config()
+}
+
 const winston = require('winston');
 
 /** Declare app variable */
