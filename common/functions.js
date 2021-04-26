d4773d6464d04599ee16a9959d66768f97c98a2d
AshrafLobo <ashraflobo@gmail.com>
2021-04-26 13:50:04 +0300

    Remove unused console.log

--- common/functions.js
+++ common/functions.js
@@ -4,7 +4,6 @@ const config = require('config');
 /** Get news article text from txt file */
 function getNewsArticle(articlePath) {
   articlePath = articlePath.replace("\\", "\/");
-  console.log("Article path", articlePath);
   return new Promise((resolve, reject) => {
     fs.readFile(articlePath, (err, article) => {
       if (err) reject(err);
