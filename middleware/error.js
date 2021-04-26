c035a027a73cf178ea2de1f979859e825ecf4b73
AshrafLobo <ashraflobo@gmail.com>
2021-04-26 18:25:06 +0300

    Some changes

--- middleware/error.js
+++ middleware/error.js
@@ -3,5 +3,5 @@ const winston = require('winston');
 module.exports = function (err, req, res, next) {
   console.log("Your error is", err.message);
   // winston.error(err.message, { metadata: err });
-  // res.status(500).send('Something failed.');
+  res.status(500).send('Something failed.');
 };
