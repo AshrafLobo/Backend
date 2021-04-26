fdadfde77b238f87204cd2e4b8caa86b306087b9
AshrafLobo <ashraflobo@gmail.com>
2021-04-26 13:12:24 +0300

    Error hack

--- common/functions.js
+++ common/functions.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const config = require('config');
 
 /** Get news article text from txt file */
 function getNewsArticle(articlePath) {
+  console.log("Article path", articlePath);
   return new Promise((resolve, reject) => {
     fs.readFile(articlePath, (err, article) => {
       if (err) reject(err);
